7be95a29e95537aff7b49752f45ff8f8b76e3741
ZZ <zhangzhang.si@gmail.com>
2018-06-20 10:01:15 -0500

    make dockerized build work

--- tensorflow/contrib/lite/examples/android/download.sh
+++ tensorflow/contrib/lite/examples/android/download.sh
@@ -1,2 +1,3 @@
-wget https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip
+# wget https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
 wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
